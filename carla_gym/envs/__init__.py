0855aa3e8177873f9826d69fdb875408fad11730
ZJJxy <00165445@zjxu.edu.cn>
2024-08-04 21:23:19 +0800

    Delete carla_gym/envs directory

--- carla_gym/envs/__init__.py
+++
@@ -1,3 +0,0 @@
-from carla_gym.envs.carla_env_v3 import CarlaGymEnv as CarlaGymEnv_v3
-from carla_gym.envs.carla_env_v2 import CarlaGymEnv as CarlaGymEnv_v2
-from carla_gym.envs.carla_env_v1 import CarlaGymEnv as CarlaGymEnv_v1
